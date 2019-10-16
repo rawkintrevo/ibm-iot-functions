488385450b6e7af98b032b2441e999f03d5c2db4
Trevor a.k.a @rawkintrevo <trevor.d.grant@gmail.com>
2019-10-16 13:21:21 -0500

    last bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='rawkintrevos-custom',
+    name='rawkintrevoscustom',
     version='0.0.1',
     packages=find_packages(),
     install_requires=['dill'
