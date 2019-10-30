b86e6e17653401539699d9ac94aa4748dc5f2dde
Trevor a.k.a @rawkintrevo <trevor.d.grant@gmail.com>
2019-10-30 07:16:02 -0500

    foo baz

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rawkintrevo_tutorial2',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=['dill'
     ],
--- update.sh
+++ update.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+# update setup.py (version number)
+# update init.py (version number)
+# git commit
+# git push
